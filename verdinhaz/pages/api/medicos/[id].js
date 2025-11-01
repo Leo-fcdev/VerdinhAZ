79b1a155c6b06d758f0d9b12faa86a298a3adecc
Leonardo Freitas <leoneryfreitas@gmail.com>
2025-10-31 23:01:47 -0300

    Api do perfil do Médico

---
+++ verdinhaz/pages/api/medicos/[id].js
@@ -0,0 +1,30 @@
+import prisma from "@/lib/prisma";
+
+export default async function handler(req, res) {
+    
+    if (req.method !== 'GET') {
+        res.setHeader('Allow', ['GET']);
+        return res.status(405).end(`Método ${req.method} não permitido`);
+    }
+
+    const { id } = req.query;
+
+    try {
+
+        const medico = await prisma.medico.findUnique({
+            where: {
+                id: parseInt(id),
+            },
+        });
+
+        if (!medico) {
+            return res.status(404).json({error : 'Médico não encontrado'});
+        } 
+
+        return res.status(200).json(medico);
+
+    } catch (error) {
+        console.error('Erro ao buscar médico:', error);
+        return res.status(500).json({ error: 'Erro interno do servidor.'});
+    }
+}
